d141a8df0e94850fd3c6b2b3b8438ab8cc4d87f8
Juan Flores Moroco <pcsijflo@upc.edu.pe>
2021-05-27 11:02:59 -0500

    Update static folder

--- src/main/java/pe/edu/upc/pandemia/service/crud/impl/LocationServiceImpl.java
+++ src/main/java/pe/edu/upc/pandemia/service/crud/impl/LocationServiceImpl.java
@@ -1,7 +1,6 @@
 package pe.edu.upc.pandemia.service.crud.impl;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
