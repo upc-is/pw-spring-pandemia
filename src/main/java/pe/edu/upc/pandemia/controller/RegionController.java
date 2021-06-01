5fbd75d1071ee7382b7070662fb0019083c97315
Juan Flores Moroco <pcsijflo@upc.edu.pe>
2021-06-01 11:58:47 -0500

    Adding list and view

--- src/main/java/pe/edu/upc/pandemia/controller/RegionController.java
+++ src/main/java/pe/edu/upc/pandemia/controller/RegionController.java
@@ -1,11 +1,13 @@
 package pe.edu.upc.pandemia.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import pe.edu.upc.pandemia.model.entity.Region;
@@ -18,7 +20,7 @@ public class RegionController {
 	@Autowired
 	private RegionService regionService;
 	
-	@GetMapping
+	@GetMapping		// GET: /regions
 	public String listar( Model model ) {
 		try {
 			List<Region> regions = regionService.getAll();
@@ -30,4 +32,20 @@ public class RegionController {
 		
 		return "regions/lista";
 	}
+	
+	@GetMapping("{id}")		// GET: /regions/{id}
+	public String findById(Model model, @PathVariable("id") Integer id) {
+		try {
+			Optional<Region> optional = regionService.findById(id);
+			if(optional.isPresent()) {
+				model.addAttribute("region", optional.get());
+				return "regions/view";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		return "";
+	}
+	
 }
