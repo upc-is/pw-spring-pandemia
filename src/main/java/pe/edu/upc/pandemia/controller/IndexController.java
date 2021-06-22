85138a1036a29e4b75f638f48f2f78049f11c4a5
Juan Flores Moroco <pcsijflo@upc.edu.pe>
2021-06-22 12:51:29 -0500

    Initializing Business Logic

--- src/main/java/pe/edu/upc/pandemia/controller/IndexController.java
+++ src/main/java/pe/edu/upc/pandemia/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.upc.pandemia.model.entity.Employee;
 import pe.edu.upc.pandemia.model.entity.Region;
 
 @Controller
@@ -14,7 +15,9 @@ public class IndexController {
 	@GetMapping
 	public String indexGet(Model model ) {
 		Region regionSearch = new Region();
+		Employee employeeSearch = new Employee();
 		model.addAttribute("regionSearch", regionSearch);
+		model.addAttribute("employeeSearch", employeeSearch);
 		return "index";
 	}
 	
--- src/main/java/pe/edu/upc/pandemia/controller/SearchController.java
+++ src/main/java/pe/edu/upc/pandemia/controller/SearchController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pe.edu.upc.pandemia.model.entity.Employee;
 import pe.edu.upc.pandemia.model.entity.Region;
+import pe.edu.upc.pandemia.service.crud.EmployeeService;
 import pe.edu.upc.pandemia.service.crud.RegionService;
 
 @Controller
@@ -18,6 +20,9 @@ public class SearchController {
 	
 	@Autowired
 	private RegionService regionService;
+	
+	@Autowired
+	private EmployeeService employeeService;
 	
 	@GetMapping("regions")
 	public String searchRegionGet(Model model, @ModelAttribute("regionSearch") Region regionSearch) {
@@ -32,6 +37,24 @@ public class SearchController {
 		}
 		
 		return "search/regions-result";
+	}
+	
+	@GetMapping("employees")
+	public String searchEmployeeGet(Model model, @ModelAttribute("employeeSearch") Employee employeeSearch) {
+		System.out.println(employeeSearch.getLastName());
+		try {
+			List<Employee> employeesFound = employeeService
+					.findByLastNameStartingWithAndFirstNameStartingWith(
+							employeeSearch.getLastName(), "");
+			
+			model.addAttribute("employeesFound", employeesFound);
+			model.addAttribute("employeeSearch", employeeSearch);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getMessage());
+		}
+		
+		return "search/employees-result";
 	}
 
 }
